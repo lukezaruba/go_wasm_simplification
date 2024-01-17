b3c7f0cd17008449f56a6e3b860f1dc49421b0b7
Luke Zaruba <lukeazaruba@gmail.com>
2024-01-16 20:27:07 -0600

    Inital version

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/lukezaruba/go_wasm_simplification
+
+go 1.21.5
+
+require (
+	github.com/golang/go v0.0.0-20240113110950-7abeefd2b1a0 // indirect
+	github.com/paulmach/orb v0.11.0 // indirect
+	go.mongodb.org/mongo-driver v1.11.4 // indirect
+)
---
+++ main.go
@@ -0,0 +1,66 @@
+// Code for WASM/JS taken from https://github.com/planetlabs/gpq
+
+package main
+
+import (
+	"syscall/js"
+
+	"github.com/paulmach/orb"
+	"github.com/paulmach/orb/geojson"
+	"github.com/paulmach/orb/simplify"
+)
+
+// Helper funcs for returning errors/values
+const (
+	errorKey = "error"
+	valueKey = "value"
+)
+
+func returnFromErrorMessage(message string) map[string]interface{} {
+	return map[string]interface{}{errorKey: message}
+}
+
+func returnFromError(err error) map[string]interface{} {
+	return returnFromErrorMessage(err.Error())
+}
+
+func returnFromValue(value interface{}) map[string]interface{} {
+	return map[string]interface{}{valueKey: value}
+}
+
+// Line GeoJSON to Simplified Version
+func ProcessGeoJSON(this js.Value, args []js.Value) any {
+
+	numBytes := args[0].Length()
+	data := make([]byte, numBytes)
+	js.CopyBytesToGo(data, args[0])
+
+	fc, err := geojson.UnmarshalFeatureCollection(data)
+	if err != nil {
+		return returnFromError(err)
+	}
+
+	simplifiedFC := geojson.NewFeatureCollection()
+
+	for _, feature := range fc.Features {
+		lineString := feature.Geometry.(orb.LineString)
+		// TODO: Add in second arg for threshold
+		simplifiedLine := simplify.DouglasPeucker(0.001).Simplify(lineString.Clone())
+		simplifiedFeature := geojson.NewFeature(simplifiedLine)
+		simplifiedFeature.Properties = feature.Properties
+		simplifiedFC.Append(simplifiedFeature)
+	}
+
+	simplifiedRawJSON, err := simplifiedFC.MarshalJSON()
+	if err != nil {
+		return returnFromError(err)
+	}
+
+	return returnFromValue(string(simplifiedRawJSON))
+}
+
+func main() {
+	js.Global().Set("ProcessGeoJSON", js.FuncOf(ProcessGeoJSON))
+
+	<-make(chan struct{}, 0)
+}
